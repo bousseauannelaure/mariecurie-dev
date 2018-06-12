5013ae79becf04b3dca9f9b515af2fe37e6b96de
Gerard <gadroher@tbscg.com>
2018-06-12 13:59:07 +0200

    Slicing integration - Custom ID

--- lib/themes/mariecurie/template.php
+++ lib/themes/mariecurie/template.php
@@ -5,3 +5,11 @@
  * Theme functions.
  */
 
+/**
+ * Implements hook_preprocess_HOOK().
+ */
+function mariecurie_preprocess_page(&$variables) {
+  if ($variables['is_front']) {
+    $variables['msc_content_id'] = ' id="home_page"';
+  }
+}
