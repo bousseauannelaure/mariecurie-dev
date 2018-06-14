a8648342b2795b36d072b7f708324ba0ae0fa18b
Gerard <gadroher@tbscg.com>
2018-06-14 16:47:42 +0200

    Slicing integration - Welcome and module component integration

--- lib/themes/mariecurie/templates/field/field--field-msc-multiple-component--msc-module-container-component.tpl.php
+++ lib/themes/mariecurie/templates/field/field--field-msc-multiple-component--msc-module-container-component.tpl.php
@@ -51,26 +51,13 @@
  * @ingroup themeable
  */
 ?>
-<div class="slider-wrapper <?php print $classes; ?>"<?php print $attributes; ?>>
-  <div class="content slider"<?php print $content_attributes; ?>>
-    <div class="arrows">
-      <a class="previous ecl-icon ecl-icon--left"></a>
-      <a class="next ecl-icon ecl-icon--right"></a>
-    </div>
-    <div class="slides">
-      <?php foreach ($items as $delta => $item): ?>
-        <?php
-          $style_attr = '';
-          if (!empty($item['entity']['paragraphs_item']) && ($slide = current($item['entity']['paragraphs_item'])) && !empty($slide['field_msc_back_img_component'])) {
-            $style_attr = ' style="background-image: url(\'' .  trim(strip_tags(drupal_render($slide['field_msc_back_img_component']))) . '\');"';
-          }
-        ?>
-        <div class="slide<?php print ($delta === 0 ? ' active': ''); ?>"<?php print $style_attr; ?>>
-          <?php print render($item); ?>
-        </div>
-      <?php endforeach; ?>
-    </div>
-    <div class="bullets"></div>
+<div class="<?php print $classes; ?>"<?php print $attributes; ?>>
+  <?php if (!$label_hidden): ?>
+    <div class="field-label"<?php print $title_attributes; ?>><?php print $label ?>:&nbsp;</div>
+  <?php endif; ?>
+  <div class="field-items module-flex-container"<?php print $content_attributes; ?>>
+    <?php foreach ($items as $delta => $item): ?>
+      <div class="field-item <?php print $delta % 2 ? 'odd' : 'even'; ?>"<?php print $item_attributes[$delta]; ?>><?php print render($item); ?></div>
+    <?php endforeach; ?>
   </div>
 </div>
-
--- lib/themes/mariecurie/templates/field/field_msc_component_title/field--field-msc-component-title--msc-module-component.tpl.php
+++ lib/themes/mariecurie/templates/field/field_msc_component_title/field--field-msc-component-title--msc-module-component.tpl.php
@@ -51,26 +51,6 @@
  * @ingroup themeable
  */
 ?>
-<div class="slider-wrapper <?php print $classes; ?>"<?php print $attributes; ?>>
-  <div class="content slider"<?php print $content_attributes; ?>>
-    <div class="arrows">
-      <a class="previous ecl-icon ecl-icon--left"></a>
-      <a class="next ecl-icon ecl-icon--right"></a>
-    </div>
-    <div class="slides">
-      <?php foreach ($items as $delta => $item): ?>
-        <?php
-          $style_attr = '';
-          if (!empty($item['entity']['paragraphs_item']) && ($slide = current($item['entity']['paragraphs_item'])) && !empty($slide['field_msc_back_img_component'])) {
-            $style_attr = ' style="background-image: url(\'' .  trim(strip_tags(drupal_render($slide['field_msc_back_img_component']))) . '\');"';
-          }
-        ?>
-        <div class="slide<?php print ($delta === 0 ? ' active': ''); ?>"<?php print $style_attr; ?>>
-          <?php print render($item); ?>
-        </div>
-      <?php endforeach; ?>
-    </div>
-    <div class="bullets"></div>
-  </div>
-</div>
-
+<?php foreach ($items as $delta => $item): ?>
+  <?php print render($item); ?>
+<?php endforeach; ?>
