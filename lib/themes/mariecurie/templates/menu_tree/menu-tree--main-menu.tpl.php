43d45ce5e8d7dc7497d95acdc6dd0efb0a0ae945
kbedra <kbedra@tbscg.com>
2018-06-18 13:56:55 +0200

    main menu template

--- lib/themes/mariecurie/templates/menu_tree/menu-tree--main-menu.tpl.php
+++ lib/themes/mariecurie/templates/menu_tree/menu-tree--main-menu.tpl.php
@@ -4,10 +4,23 @@
  * @file
  * Contains template file.
  */
-$variables['atomium']['attributes']['wrapper']
-    ->append('class', 'ecl-col');
-?>
-<div <?php print $atomium['attributes']['wrapper']; ?>>
-    <ul class="ecl-navigation-menu__links ecl-list ecl-list--unstyled"><?php print render($tree); ?></ul>
-</div>
+$element = $variables['element'];
 
+if ($element['#below']) {
+    $variables['atomium']['attributes']['wrapper']
+        ->append('class', 'ecl-col');
+    ?>
+
+    <div <?php print $atomium['attributes']['wrapper']; ?>>
+        <ul class="ecl-navigation-menu__links ecl-list ecl-list--unstyled"><?php print render($tree); ?></ul>
+    </div>
+
+    <?php
+    } else {
+    ?>
+
+    <li<?php print $atomium['attributes']['wrapper']; ?>><?php print render($output); ?><?php print render($sub_menu); ?></li>
+
+    <?php
+    }
+}
