7efa51b25750510078c159f183330422dad961b7
Gerard <gadroher@tbscg.com>
2018-06-19 15:37:22 +0200

    Fix module entity renderization

--- lib/themes/mariecurie/templates/field/field--field-msc-desc--msc-module.tpl.php
+++ lib/themes/mariecurie/templates/field/field--field-msc-desc--msc-module.tpl.php
@@ -51,8 +51,6 @@
  * @ingroup themeable
  */
 ?>
-<!--
-
 <div class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <?php if (!$label_hidden): ?>
     <div class="field-label"<?php print $title_attributes; ?>><?php print $label ?>:&nbsp;</div>
--- lib/themes/mariecurie/templates/node/node--msc-module.tpl.php
+++ lib/themes/mariecurie/templates/node/node--msc-module.tpl.php
@@ -8,8 +8,7 @@
 <div class="row mb-100">
     <div class="col-xs-12 col-md-6 col-md-push-6 mb-30">
         <div class="video raised">
-            <iframe type="text/html"
-                    src="http://www.youtube.com/embed/M7lc1UVf-VE"></iframe>
+          <?php print render($content['field_msc_video']); ?>
         </div>
     </div>
     <div class="col-xs-12 col-md-6 col-md-pull-6">
