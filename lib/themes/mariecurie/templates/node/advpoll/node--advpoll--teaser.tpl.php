f0503e3f0a1a6ac8d345bc28cb329d6642032913
Gerard <gadroher@tbscg.com>
2018-06-20 16:20:33 +0200

    Theme file for teaser display of poll

--- lib/themes/mariecurie/templates/node/advpoll/node--advpoll--teaser.tpl.php
+++ lib/themes/mariecurie/templates/node/advpoll/node--advpoll--teaser.tpl.php
@@ -5,10 +5,9 @@
  * Contains template file.
  */
 ?>
-<a href="<?php print $node_url; ?>">
-  <div class="poll-container raised">
-    <div class="poll-header">
-      <?php print $title; ?>
-    </div>
+<a href="<?php print $node_url; ?>" class="ecl-link ecl-list-item__link">
+  <div class="ecl-list-item__body">
+    <p class="text-blue-light ecl-heading ecl-heading--h3"><?php print $title; ?></p>
+    <?php print render($content['body']); ?>
   </div>
 </a>
