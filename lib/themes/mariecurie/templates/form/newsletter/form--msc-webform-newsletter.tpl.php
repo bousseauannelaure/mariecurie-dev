be647825936178a0f2a34047222b65b716199dab
Gerard <gadroher@tbscg.com>
2018-06-25 12:20:18 +0200

    change order of newsletter form elements

--- lib/themes/mariecurie/templates/form/newsletter/form--msc-webform-newsletter.tpl.php
+++ lib/themes/mariecurie/templates/form/newsletter/form--msc-webform-newsletter.tpl.php
@@ -8,7 +8,12 @@
 <div class="newsletter-frame ecl-u-pa-xxl">
   <form<?php print $atomium['attributes']['element']->append('class', 'ecl-form'); ?>>
     <div class="text-center">
-      <?php print render($element['#children']); ?>
+      <?php print render($element['submitted']['register_newsletter']); ?>
+      <?php print render($element['submitted']['email']); ?>
+      <?php print render($element['actions']); ?>
+      <?php print render($element['submitted']['msc_gdpr']); ?>
     </div>
+    <?php print render($element['form_id']); ?>
+    <?php print render($element['form_build_id']); ?>
   </form>
 </div>
