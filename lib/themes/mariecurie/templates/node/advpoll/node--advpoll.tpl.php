9bc41f26fb91142330f340f385b6fe0e4fc65c72
kbedra <kbedra@tbscg.com>
2018-06-26 09:31:58 +0200

    polls WIP

--- lib/themes/mariecurie/templates/node/advpoll/node--advpoll.tpl.php
+++ lib/themes/mariecurie/templates/node/advpoll/node--advpoll.tpl.php
@@ -6,19 +6,18 @@
  */
 ?>
 <h1 class="ecl-heading ecl-heading--h1">Polls</h1>
-<div class="ecl-row">
-  <div class="ecl-col">
-    <span class="article-title text-purple">Poll results</span>
-    <?php print render($content['body']); ?>
-  </div>
-</div>
+
 
 <div class="ecl-row">
-  <div class="ecl-col">
-    <div class="poll-container raised">
-      <div class="poll-body">
-        <?php print render($content['advpoll_choice']); ?>
-      </div>
+    <div class="ecl-col">
+        <div class="poll-container raised">
+            <div class="poll-header">
+                Today's quick poll
+            </div>
+            <div class="poll-body">
+                <?php print render($content['body']); ?>
+                <?php print render($content['advpoll_choice']); ?>
+            </div>
+        </div>
     </div>
-  </div>
 </div>
